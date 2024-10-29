411d4870c158287d5dc6d48f36847e2e941cc912
Natalia Iacono <167995391+NataliaIacono@users.noreply.github.com>
2024-10-29 14:57:14 +0000

    correcion de variable de entorno BACKEND_URL

--- src/front/js/store/flux.js
+++ src/front/js/store/flux.js
@@ -25,7 +25,7 @@ const getState = ({ getStore, getActions, setStore }) => {
 			getMessage: async () => {
 				try {
 					// fetching data from the backend
-					const resp = await fetch(process.env.REACT_APP_BACKEND_URL + "/api/hello")
+					const resp = await fetch(process.env.BACKEND_URL + "/api/hello")
 					const data = await resp.json()
 					setStore({ message: data.message })
 					// don't forget to return something, that is how the async resolves
@@ -54,7 +54,8 @@ const getState = ({ getStore, getActions, setStore }) => {
 			Usuarios: async () => {
 				const token = localStorage.getItem('token');
 				try {
-					const response = await fetch(`${process.env.REACT_APP_BACKEND_URL}/usuarios`, {
+					const response = await fetch(process.env.BACKEND_URL + '/api/usuarios', {
+						             
 						method: 'GET',
 						headers: {
 							'Content-Type': 'application/json',
